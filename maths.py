85feac9449d4a2fee6ad115241cf2ced86b6a3bd
Oscar McGlone <147001927+oscarmcgl@users.noreply.github.com>
2024-06-28 23:05:28 +0100

    Worded Addition

--- maths.py
+++ maths.py
@@ -6,8 +6,7 @@ fruitpl = ['apples', 'bananas', 'oranges', 'pears', 'grapes', 'mangoes', 'kiwis'
 item = ['pen', 'pencil', 'rubber', 'ruler', 'notebook', 'folder', 'scissors', 'glue stick']
 itempl = ['pens', 'pencils', 'rubbers', 'rulers', 'notebooks', 'folders', 'scissors', 'glue sticks']
 names = ['Alistair','Lara','Hannah','Ollie','Zoe','Ellie','Jess','Albie','Alex']
-add_questions = [
-    '{name} has {num1} {choice1} and {num2} {choice2}. How many {choice3} does {name} have in total?']
+add_questions = '{name} has {num1} {choice1} and {name2} has {num2} {choice2}. How many {choice3} do {name} and {name2} have in total?'
 sub_questions = [
     '{name} has {num1} {choice1} and gives {num2} {choice2} away. How many {choice3} does {name} have left?']
 div_questions =[
@@ -35,7 +34,27 @@ def worded_basic():
     choice = random.randint(1, 2)
     if choice == 1: # addition
         name = random.choice(names)
+        name2 = random.choice(names)
         choices = random.randint(0,len(fruit)-1)
-        #! work out whether singular or plural etc.
+        
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        answer = num1 + num2
 
+        if num1 == 1:
+            choice1 = fruit[choices]
+        else:
+            choice1 = fruitpl[choices]
+        
+        if num2 == 1:
+            choice2 = fruit[choices]
+        else:
+            choice2 = fruitpl[choices]
 
+        choice3 = fruitpl[choices]
+
+        question = add_questions.format(name=name, name2=name2, num1=num1, num2=num2, choice1=choice1, choice2=choice2, choice3=choice3)
+        print(question, answer)
+
+    else: #subtraction
+        pass 
