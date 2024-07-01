88064f2df56b378df13dba7397ae0dbb89904809
Oscar McGlone <147001927+oscarmcgl@users.noreply.github.com>
2024-07-01 22:08:18 +0100

    Add Plant Initialisation

--- plants.py
+++ plants.py
@@ -1,4 +1,5 @@
 import json 
+import userface as uf
 
 
 def retrieve_data(num, info):
@@ -14,6 +15,22 @@ def name(num):
 def pname(num):
     retrieve_data(num, 'proper_name')
 
+
+def add_plant(type):
+
+    if uf.checklogin() is False:
+        print('Not logged in.')
+        return None
+    else:
+        with open('database/info.json', 'r') as db:
+            data = json.load(db)
+            currentid = data[0]['currentid'] #! This doesn't work??
+            print(currentid)
+
+
+
+
+    
 
 
 
