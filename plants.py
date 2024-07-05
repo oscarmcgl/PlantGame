be10706288f7d318cf41ad5a38ed624bdd3b70f7
Oscar McGlone <147001927+oscarmcgl@users.noreply.github.com>
2024-07-05 21:13:36 +0100

    Fixed add_plant(). Added ability to remove plants with presence check

--- plants.py
+++ plants.py
@@ -24,9 +24,52 @@ def add_plant(type):
     else:
         with open('database/info.json', 'r') as db:
             data = json.load(db)
-            currentid = data[0]['currentid'] #! This doesn't work??
-            print(currentid)
 
+            if len(data)-1 < type:
+                print('Plant not found in database.')
+                return None
+            
+            else:
+                plantname = data[type]['name']
+
+        with open('database/users.json', 'r') as db:
+            data = json.load(db)
+            currentid = data[0]['currentid'] 
+            owned_plants = data[currentid]['owned_plants']
+            owned_plants.append(type)
+            data[currentid]['owned_plants'] = owned_plants
+            with open('database/users.json', 'w') as db:
+                json.dump(data, db, indent=4)
+                print(plantname + ' added to your collection.')
+
+def remove_plant(type):
+
+    if uf.checklogin() is False:
+        print('Not logged in.')
+        return None
+    else:
+        with open('database/info.json', 'r') as db:
+            data = json.load(db)
+
+            if len(data)-1 < type:
+                print('Plant not found in database.')
+                return None
+            
+            else:
+                plantname = data[type]['name']
+
+        try:
+            with open('database/users.json', 'r') as db:
+                data = json.load(db)
+                currentid = data[0]['currentid']
+                owned_plants = data[currentid]['owned_plants']
+                owned_plants.remove(type)
+                data[currentid]['owned_plants'] = owned_plants
+                with open('database/users.json', 'w') as db:
+                    json.dump(data, db, indent=4)
+                    print(plantname + ' removed from your collection.')
+        except:
+            print('Plant not found in database.')
 
 
 
